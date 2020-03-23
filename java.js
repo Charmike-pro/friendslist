1e8c7bf7456b0f0de0354a0927edf27aa8626837
Char Mike <storagewars999@gmail.com>
2020-03-23 17:23:24 +0200

    tein sivusta toiminnallisen ja tein .css-tiedoston!

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-            function addLi()
+ function addLi()
             {
 
                 var txtVal = document.getElementById('txtVal').value,
@@ -9,4 +9,6 @@
                  liNode.appendChild(txtNode);
                  listNode.appendChild(liNode);
             
+            
+
             }
