d569d9d58d4fbe9ce7ab98cdc24afdcf4ee853a0
Char Mike <storagewars999@gmail.com>
2020-03-24 09:10:52 +0200

    Rajoitin li-määrän kymmeneen.

--- java.js
+++ java.js
@@ -1,14 +1,20 @@
  function addLi()
             {
-
+               
+                
                 var txtVal = document.getElementById('txtVal').value,
                     listNode = document.getElementById('list'),
+                    
                     liNode = document.createElement("LI"),
                     txtNode = document.createTextNode(txtVal);
-            
                  liNode.appendChild(txtNode);
                  listNode.appendChild(liNode);
-            
-            
+                
+                 var max = 10;
+                 $('ul, ol').each(function(){
+                   $(this).find('li, ol').each(function(index){
+                     if(index >= max) $(this).hide()
+                   })
+                 })
 
             }
